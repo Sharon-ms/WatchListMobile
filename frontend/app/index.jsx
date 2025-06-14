5c5b54d7cd92bed2e2ff0afbe79ca13bc06c54de
Sharon-ms <mgegngi@gmail.com>
2025-06-14 21:28:59 +0300

    check

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,51 +1,51 @@
 import axios from "axios"
 import { useState } from "react"
-import {useRouter} from "expo-router"
+import { useRouter } from "expo-router"
 import { SafeAreaView, Button, Alert, View, Text, TextInput, TouchableOpacity } from "react-native"
 
 export default function loginPage() {
     const router = useRouter()
-    const [user, setUser] = useState({"userName":"", "password":""})
-    
+    const [user, setUser] = useState({ "userName": "", "password": "" })
+
     return (
         <SafeAreaView>
-           <TextInput placeholder="user name"
-           value={user.userName}
-           onChangeText={(text)=>setUser(prev =>({...prev, userName: text}))}
-           />
+            <TextInput placeholder="user name"
+                value={user.userName}
+                onChangeText={(text) => setUser(prev => ({ ...prev, userName: text }))}
+            />
 
             <TextInput placeholder="password"
-           value={user.password}
-           onChangeText={(text)=>setUser(prev =>({...prev, password: text}))}
-           />
+                value={user.password}
+                onChangeText={(text) => setUser(prev => ({ ...prev, password: text }))}
+            />
 
-           <Button title="Log in"
-           onPress={async()=>{
-                try{
-                    const res = await axios.get(`http://192.168.123.156:3000/users/${user.userName}`);
-                    const hasUser = res.data;
-                     if(!hasUser){
-                        Alert.alert("you don't have an account")
+            <Button title="Log in"
+                onPress={async () => {
+                    try {
+                        const res = await axios.get(`http://192.168.123.156:3000/users/${user.userName}`);
+                        const hasUser = res.data;
+                        if (!hasUser) {
+                            Alert.alert("you don't have an account")
+                        }
+                        else {
+                            hasUser.password === user.password ? router.push(`/profile/${hasUser.name}`) :
+                                Alert.alert("wrong password")
+                        }
+                    } catch (err) {
+                        if (err.response && err.response.status === 404) {
+                            Alert.alert("you don't have an account")
+                        }
+                        else {
+                            Alert.alert("connection error")
+                        }
                     }
-                    else{
-                        hasUser.password === user.password ?  router.push(`/profile/${hasUser.name}`) :
-                        Alert.alert("wrong password")
-                    }
-                }catch(err){
-                    if(err.response && err.response.status === 404){
-                        Alert.alert("you don't have an account")
-                    }
-                    else{
-                        Alert.alert("connection error")
-                    }
-                }   
-           }}/>
+                }} />
 
-           <TouchableOpacity onPress={()=>{
+            <TouchableOpacity onPress={() => {
                 router.push("/registerPage")
-           }}>
+            }}>
                 <Text>Don't have an account? Sign Up</Text>
-           </TouchableOpacity>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
--- frontend/app/registerPage.jsx
+++ frontend/app/registerPage.jsx
@@ -3,54 +3,54 @@ import { useRouter } from "expo-router";
 import { useState } from "react";
 import { SafeAreaView, TextInput, Button, Alert } from "react-native";
 
-export default function registerPage(){
+export default function registerPage() {
     const router = useRouter()
-    const [newUser, setNewUser] = useState({"name":"", "userName":"", "password":""})
+    const [newUser, setNewUser] = useState({ "name": "", "userName": "", "password": "" })
     //http://192.168.150.128:3000/users
     async function addUser(user) {
-        try{
+        try {
             const hasUser = await axios.get(`http://192.168.123.156:3000/users/${user.userName}`);
             Alert.alert("oops", "this username already in use");
             return;
-        }catch(err){
-            if(err.response?.status === 404){
-                try{
+        } catch (err) {
+            if (err.response?.status === 404) {
+                try {
                     const res = await axios.post(`http://192.168.123.156:3000/users`, user);
                     Alert.alert("Success", "user create");
-                     router.push("/");
-                } catch(createErr){
+                    router.push("/");
+                } catch (createErr) {
                     Alert.alert("Error", "sign up failed")
                 }
             }
-            else{
+            else {
                 Alert.alert("Error", err.response?.data.message || "connection failed")
             }
         }
     }
 
 
-    return(
+    return (
         <SafeAreaView>
             <TextInput placeholder="name"
-            value={newUser.name}
-            onChangeText={(text)=> setNewUser(prev=> ({...prev, name: text}))}
+                value={newUser.name}
+                onChangeText={(text) => setNewUser(prev => ({ ...prev, name: text }))}
             />
 
             <TextInput placeholder="user name"
-            value={newUser.userName}
-            onChangeText={(text)=> setNewUser(prev=> ({...prev, userName: text}))}
+                value={newUser.userName}
+                onChangeText={(text) => setNewUser(prev => ({ ...prev, userName: text }))}
             />
 
             <TextInput placeholder="password"
-            value={newUser.password}
-            onChangeText={(text)=> setNewUser(prev=> ({...prev, password: text}))}
+                value={newUser.password}
+                onChangeText={(text) => setNewUser(prev => ({ ...prev, password: text }))}
             />
 
             <Button title="sign up"
-            onPress={async()=>{
-                await addUser(newUser);
-            }
-            }
+                onPress={async () => {
+                    await addUser(newUser);
+                }
+                }
             />
         </SafeAreaView>
     )
